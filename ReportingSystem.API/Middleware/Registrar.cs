3304b3eb9aff16f525b538a656eb90c097b90fa8
imrancs10 <imran.cs10@gmail.com>
2023-10-30 08:45:23 +0530

    db cred changed

--- ReportingSystem.API/Middleware/Registrar.cs
+++ ReportingSystem.API/Middleware/Registrar.cs
@@ -28,7 +28,7 @@ namespace ReportingSystem.API.Middleware
         public static IServiceCollection RegisterDataServices(this IServiceCollection services)
         {
             //var DefaultConnection = "workstation id=mssql-88450-0.cloudclusters.net,12597;TrustServerCertificate=true;user id=LaBeachUser;pwd=Gr8@54321;data source=mssql-88450-0.cloudclusters.net,12597;persist security info=False;initial catalog=KaashiYatri";
-            var DefaultConnection = "workstation id=mssql-88450-0.cloudclusters.net,12597;TrustServerCertificate=true;user id=ReportingSystem;pwd=Gr8@12345;data source=mssql-88450-0.cloudclusters.net,12597;persist security info=False;initial catalog=ReportingSystem";
+            var DefaultConnection = "workstation id=mssql-88450-0.cloudclusters.net,12597;TrustServerCertificate=true;user id=LaBeachUser;pwd=Gr8@54321;data source=mssql-88450-0.cloudclusters.net,12597;persist security info=False;initial catalog=ReportingSystem";
             services.AddDbContext<ReportingSystemContext>(
                 options => { options.UseSqlServer(DefaultConnection); }
             );
