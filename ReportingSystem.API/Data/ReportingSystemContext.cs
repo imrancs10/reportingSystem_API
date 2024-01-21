c7aaacd8d7cd8893a2385f85b18b98059a7bfb9f
Amendra Dutt <amendra.dutt@gmail.com>
2024-01-21 20:20:44 +0530

    connection change

--- ReportingSystem.API/Data/ReportingSystemContext.cs
+++ ReportingSystem.API/Data/ReportingSystemContext.cs
@@ -17,7 +17,7 @@ namespace ReportingSystem.API.Data
             Configuration = configuration;
             _httpContextAccessor = httpContextAccessor;
             //_defaultConnection = "workstation id=mssql-88450-0.cloudclusters.net,12597;TrustServerCertificate=true;user id=LaBeachUser;pwd=Gr8@54321;data source=mssql-88450-0.cloudclusters.net,12597;persist security info=False;initial catalog=KaashiYatri";
-            _defaultConnection = "workstation id=mssql-88450-0.cloudclusters.net,12597;TrustServerCertificate=true;user id=LaBeachUser;pwd=Gr8@54321;data source=mssql-88450-0.cloudclusters.net,12597;persist security info=False;initial catalog=ReportingSystem_Test";
+            _defaultConnection = "workstation id=mssql-88450-0.cloudclusters.net,12597;TrustServerCertificate=true;user id=LaBeachUser;pwd=Gr8@54321;data source=mssql-88450-0.cloudclusters.net,12597;persist security info=False;initial catalog=ReportingSystem";
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
